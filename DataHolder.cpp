33cf374cde65951d6c0d3a98ac106c206fad7933
hibo98 <niklasmerkelt@mail.de>
2018-11-01 14:05:56 +0100

    Don't create JsonRequest on heap

--- DataHolder.cpp
+++ DataHolder.cpp
@@ -57,10 +57,10 @@ QMap<int, QMap<int, Link*>*> DataHolder::getLinks() {
 }
 
 void DataHolder::requestAPI() {
-    JsonRequest* request = new JsonRequest(QUrl("http://api.freifunk-dresden.de/freifunk-niklas-hopglass.json"));
-    connect(request, &JsonRequest::result, this, &DataHolder::processAPI);
-    connect(request, &JsonRequest::error, this, &DataHolder::processAPIError);
-    request->run();
+    JsonRequest request(QUrl("http://api.freifunk-dresden.de/freifunk-niklas-hopglass.json"));
+    connect(&request, &JsonRequest::result, this, &DataHolder::processAPI);
+    connect(&request, &JsonRequest::error, this, &DataHolder::processAPIError);
+    request.run();
 }
 
 void DataHolder::processAPI(QJsonDocument doc) {
